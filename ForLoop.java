d4690f5b385425f98878bb18d82f0d5475a9d9e6
Madhurii-Patil <patilmadhuri7279@gmail.com>
2024-06-11 15:10:04 +0530

    This is about Java Program

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+package com.statement;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		for(int i=10;i>=0;i--)
+			if(i%2==0)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
